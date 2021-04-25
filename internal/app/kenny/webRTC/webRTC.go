9722a95cede7449cbacf748d6cee77fec87170ad
Korosh <korosh.roohi9731@gmail.com>
2021-04-25 12:13:30 +0430

    Add an API for creating webRTC configs

--- internal/app/kenny/webRTC/webRTC.go
+++ internal/app/kenny/webRTC/webRTC.go
@@ -4,3 +4,14 @@ import (
 	"github.com/pion/webrtc/v3"
 )
 
+// make config for stun & turn servers (no turn server yet :))
+func makeConfig() webrtc.Configuration {
+	config := webrtc.Configuration {
+		ICEServers: []webrtc.ICEServer {
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"},
+			},
+		},
+	}
+	return config
+}
