018121b46610c500bfe78986f7830d1fc35fc3d8
Mohammad Fatemi <mr.smf8@gmail.com>
2021-04-26 02:18:50 +0430

    refactor: Add comments for exported structs and interfaces.

--- pkg/audio/audio.go
+++ pkg/audio/audio.go
@@ -1,10 +1,14 @@
 package audio
 
+// API represents the wrapper for playback and recording.
 type API struct {
 	Player   Player
 	Recorder Recorder
 }
 
+// Player is a general interface for audio playback.
+// call OpenStream for each media stream and close it
+// after you're done using it.
 type Player interface {
 	OpenStream() (int, chan<- [][]int16, error)
 	Play(streamID int) error
@@ -12,6 +16,9 @@ type Player interface {
 	CloseStream(streamID int) error
 }
 
+// Recorder is a general interface for audio recording.
+// call OpenStream for each media input (i.e. microphone and system sound)
+// and close it after you're done using it.
 type Recorder interface {
 	OpenStream() (int, <-chan [][]int16, error)
 	Record(streamID int) error
