041160f1996546e021ef17ac043b9cc6df8b9304
Mohammad Fatemi <mr.smf8@gmail.com>
2021-05-06 02:22:16 +0430

    feat: Complete portaudio implementation of a recorder

--- pkg/audio/portaudio/device.go
+++ pkg/audio/portaudio/device.go
@@ -15,10 +15,13 @@ const (
 	RecordDeviceType DeviceType = iota
 	// PlayDeviceType is for audio output device.
 	PlayDeviceType
+
+	// DeviceNameDefault is the default devicename which lets port audio api to choose default audio device.
+	DeviceNameDefault = "default"
 )
 
 var (
-	//ErrDeviceNotFound occurs when device with given info does not exist in the system.
+	// ErrDeviceNotFound occurs when device with given info does not exist in the system.
 	ErrDeviceNotFound = errors.New("failed to find audio device")
 )
 
@@ -27,6 +30,33 @@ type StreamSettings struct {
 	SampleRate      float64
 	Channels        int
 	FramesPerBuffer int
+}
+
+// because we may have multiple audio sources(i.e multiple speakers),
+// we represent each one with a audioStream instance.
+type audioStream struct {
+	stream      *portaudio.Stream
+	buffer      []int16
+	bufferIndex int
+}
+
+// Init must be called before any further usage on portaudio package
+func Init() error {
+	err := portaudio.Initialize()
+	if err != nil {
+		return fmt.Errorf("failed to initialize portaudio: %w", err)
+	}
+
+	return nil
+}
+
+// Cleanup must be called when you are done using portaudio package.
+func Cleanup() error {
+	if err := portaudio.Terminate(); err != nil {
+		return fmt.Errorf("failed to terminate portaudio: %w", err)
+	}
+
+	return nil
 }
 
 func newStreamParam(
@@ -45,23 +75,69 @@ func newStreamParam(
 		}
 	}
 
+	if deviceName == DeviceNameDefault {
+		if deviceType == RecordDeviceType {
+			device = defaultHostAPI.DefaultInputDevice
+		} else {
+			device = defaultHostAPI.DefaultOutputDevice
+		}
+	}
+
 	if device == nil {
 		return nil, ErrDeviceNotFound
 	}
 
-	var param portaudio.StreamParameters
+	deviceParameters := portaudio.StreamDeviceParameters{
+		Device:   device,
+		Channels: settings.Channels,
+	}
+	sp := portaudio.StreamParameters{
+		SampleRate:      settings.SampleRate,
+		FramesPerBuffer: settings.FramesPerBuffer,
+		Flags:           portaudio.ClipOff,
+	}
 
 	switch deviceType {
 	case RecordDeviceType:
-		param = portaudio.LowLatencyParameters(device, nil)
-		param.Input.Channels = settings.Channels
+		deviceParameters.Latency = device.DefaultLowInputLatency
+		sp.Input = deviceParameters
 	case PlayDeviceType:
-		param = portaudio.LowLatencyParameters(nil, device)
-		param.Output.Channels = settings.Channels
+		deviceParameters.Latency = device.DefaultLowOutputLatency
+		sp.Output = deviceParameters
 	}
 
-	param.FramesPerBuffer = settings.FramesPerBuffer
-	param.SampleRate = settings.SampleRate
+	return &sp, nil
+}
 
-	return &param, nil
+// NewStreamParam creates a new output/input stream which is ready to use.
+// For temporarily pausing record/play use Stop() and to finish using stream call Close().
+func NewStreamParam(s *StreamSettings, device *portaudio.DeviceInfo,
+	deviceType DeviceType) (portaudio.StreamParameters, error) {
+	deviceParameters := portaudio.StreamDeviceParameters{
+		Device:   device,
+		Channels: s.Channels,
+		Latency:  device.DefaultLowInputLatency,
+	}
+	sp := portaudio.StreamParameters{
+		SampleRate:      s.SampleRate,
+		FramesPerBuffer: s.FramesPerBuffer,
+		Flags:           portaudio.ClipOff,
+	}
+
+	if deviceType == RecordDeviceType {
+		sp.Input = deviceParameters
+	} else if deviceType == PlayDeviceType {
+		sp.Output = deviceParameters
+	}
+
+	return sp, nil
+}
+
+func openStream(buffer []int16, params portaudio.StreamParameters) (*portaudio.Stream, error) {
+	stream, err := portaudio.OpenStream(params, buffer)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open stream: %w", err)
+	}
+
+	return stream, nil
 }
--- pkg/audio/portaudio/recorder.go
+++ pkg/audio/portaudio/recorder.go
@@ -3,34 +3,24 @@ package portaudio
 import (
 	"fmt"
 
+	"github.com/sirupsen/logrus"
+
 	"github.com/gordonklaus/portaudio"
 )
 
 // PORecorder represents portaudio recorder. it implements audio.Recorder interface.
 type PORecorder struct {
 	streamParams portaudio.StreamParameters
-	streams      []recordStream
-}
-
-// because we may have multiple audio sources(i.e multiple speakers),
-// we represent each one with a recordStream instance.
-type recordStream struct {
-	stream  *portaudio.Stream
-	channel chan [][]int16
-	buffer  [][]int16
+	streams      []*audioStream
 }
 
 // NewRecorder creates a new PORecorder instance with given StreamSettings.
 // deviceName must be a valid portaudio device name from devices command.
+// use DeviceNameDefault as deviceName to use system's default device.
 func NewRecorder(deviceName string, settings StreamSettings) (*PORecorder, error) {
 	param, err := newStreamParam(deviceName, RecordDeviceType, settings)
 	if err != nil {
 		return nil, err
-	}
-
-	buffer := make([][]int16, settings.Channels)
-	for i := range buffer {
-		buffer[i] = make([]int16, settings.FramesPerBuffer)
 	}
 
 	return &PORecorder{
@@ -40,35 +30,27 @@ func NewRecorder(deviceName string, settings StreamSettings) (*PORecorder, error
 
 // OpenStream opens a new stream on the device and assigns an ID to it.
 // you can use this ID to record, pause or stop the stream.
-// you can receive audio data (first dimension is the number of audio channels) from
-// the channel after you call Record.
-//
-// Make sure to close the stream after you're done using it.
-func (p *PORecorder) OpenStream() (int, <-chan [][]int16, error) {
-	buffer := make([][]int16, p.streamParams.Input.Channels)
-	for i := range buffer {
-		buffer[i] = make([]int16, p.streamParams.FramesPerBuffer)
-	}
+// This function will also Start the portaudio stream so Make sure to close it after you are done using it.
+func (p *PORecorder) OpenStream() (int, error) {
+	buffer := make([]int16, p.streamParams.Input.Channels*p.streamParams.FramesPerBuffer)
 
-	stream, err := portaudio.OpenStream(p.streamParams, buffer)
+	stream, err := openStream(buffer, p.streamParams)
 	if err != nil {
-		return -1, nil, fmt.Errorf("failed to open stream: %w", err)
+		return -1, err
 	}
 
-	if err := stream.Start(); err != nil {
-		return -1, nil, fmt.Errorf("failed to start stream: %w", err)
-	}
-
-	ch := make(chan [][]int16)
 	id := len(p.streams)
-	s := recordStream{
-		stream:  stream,
-		channel: ch,
-		buffer:  buffer,
+	s := &audioStream{
+		stream: stream,
+		buffer: buffer,
 	}
 	p.streams = append(p.streams, s)
 
-	return id, ch, nil
+	if err := stream.Start(); err != nil {
+		return -1, fmt.Errorf("failed to start record %d: %w", id, err)
+	}
+
+	return id, nil
 }
 
 // CloseStream closes the stream and it's data channel.
@@ -77,12 +59,12 @@ func (p *PORecorder) CloseStream(streamID int) error {
 		return fmt.Errorf("failed to close stream %d: %w", streamID, err)
 	}
 
-	close(p.streams[streamID].channel)
-
 	return nil
 }
 
 // PauseRecord pauses The record making next call to stream.Read() return an error.
+//
+//
 func (p *PORecorder) PauseRecord(streamID int) error {
 	if err := p.streams[streamID].stream.Stop(); err != nil {
 		return fmt.Errorf("failed to pause record %d: %w", streamID, err)
@@ -91,26 +73,26 @@ func (p *PORecorder) PauseRecord(streamID int) error {
 	return nil
 }
 
-// Record starts audio recording inside a go routine.
-// sending captured data into stream's channel.
-func (p *PORecorder) Record(streamID int) error {
-	recordStream := p.streams[streamID]
-	if err := recordStream.stream.Start(); err != nil {
+// ResumeRecord starts stream processing in portaudio. you MUST call this between a PauseRecord and Record call.
+//
+// calling this function for an already started stream will cause error.
+func (p *PORecorder) ResumeRecord(streamID int) error {
+	if err := p.streams[streamID].stream.Start(); err != nil {
 		return fmt.Errorf("failed to start record %d: %w", streamID, err)
 	}
 
-	var err error
-
-	go func() {
-		for {
-			if err = recordStream.stream.Read(); err != nil {
-				err = fmt.Errorf("failed to read input stream %d: %w", streamID, err)
-				break
-			}
-
-			recordStream.channel <- recordStream.buffer
-		}
-	}()
-
 	return nil
+}
+
+// Record reads an audio chunk from device and returns it.
+//
+// the returned slice will be changed in next calls to Record, So use copy to store it.
+func (p *PORecorder) Record(streamID int) ([]int16, error) {
+	recordStream := p.streams[streamID]
+
+	if err := recordStream.stream.Read(); err != nil {
+		logrus.Errorf("failed to read input stream %d: %w", streamID, err)
+	}
+
+	return recordStream.buffer, nil
 }
